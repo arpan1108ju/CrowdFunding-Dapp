b7a525aa78d901d25ad6f8cc006e5c156adf4ff5
Prasenjit20000 <prasend1008@gmail.com>
2024-12-23 12:48:12 +0530

    add toast

--- client1/src/App.jsx
+++ client1/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 //Prasenjitgit 
+// add toastify
 import ConnectWalletButton from "./components/ConnectWalletButton";
 // import { requestAccount } from "./utils/contractServices";
 
