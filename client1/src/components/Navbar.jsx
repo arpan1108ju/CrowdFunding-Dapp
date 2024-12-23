b6c42ae0790c3d0f0496ed1322689181dfded320
Prasenjit20000 <prasend1008@gmail.com>
2024-12-23 23:15:08 +0530

    search update

--- client1/src/components/Navbar.jsx
+++ client1/src/components/Navbar.jsx
@@ -22,12 +22,18 @@ const Navbar = () => {
         navigate(`/campaign-details/${campaign?.title}`, { state: JSON.stringify(campaign) })
       }
   }
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
 
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input
+        onKeyDown={(e)=>handleKeyPress(e)}
         onChange={(e) => setSearchFieldValue(e.target.value)}
         type="text" value={searchFieldValue} placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
         
