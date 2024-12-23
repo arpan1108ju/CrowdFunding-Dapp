ca99e99ff3b089e47abe43ae5e6fc15b7f13cca5
Prasenjit20000 <prasend1008@gmail.com>
2024-12-24 01:24:47 +0530

    1:24

--- client1/src/components/FundCard.jsx
+++ client1/src/components/FundCard.jsx
@@ -3,17 +3,28 @@ import React from 'react';
 import { tagType, thirdweb } from '../assets';
 import { daysLeft } from '../utils';
 
-const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
+const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick, canceled, withdrawn }) => {
   const remainingDays = daysLeft(deadline);
-  
+
   return (
     <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
-      <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
+      <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]" />
 
       <div className="flex flex-col p-4">
-        <div className="flex flex-row items-center mb-[18px]">
-          <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
-          <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
+        <div className="flex flex-row justify-between mb-[18px]">
+          <div className="flex flex-row items-center">
+            <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain" />
+            <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
+          </div>
+          {!canceled ? (
+            remainingDays > 0 ? (
+              <span className="ml-[12px] font-epilogue font-bold text-[18px] text-[#FFFF00]">Ongoing</span>
+            ) : (
+              <span className="ml-[12px] font-epilogue font-bold text-[18px] text-[#28a745]">Completed</span>
+            )
+          ) : (
+            <span className="ml-[12px] font-epilogue font-bold text-[18px] text-[#dc3545]">Canceled</span>
+          )}
         </div>
 
         <div className="block">
@@ -34,7 +45,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
 
         <div className="flex items-center mt-[20px] gap-[12px]">
           <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
-            <img src={thirdweb} alt="user" className="w-1/2 h-1/2 object-contain"/>
+            <img src={thirdweb} alt="user" className="w-1/2 h-1/2 object-contain" />
           </div>
           <p className="flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate">by <span className="text-[#b2b3bd]">{owner}</span></p>
         </div>
